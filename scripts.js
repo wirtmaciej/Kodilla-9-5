333180539bb89118c221ed767bc70ae84efb5095
Maciej Rut <wirtmaciej@gmail.com>
2017-08-14 14:47:34 +0200

    scripts modified

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ var buttons = document.getElementsByClassName('button');
 var buttonsLength = buttons.length;
 
 for (var i = 0; i < buttonsLength; i++) {
-	buttons.innerText = 'Hello!';
-	alert(buttons.innerText);
-	console.log(buttons.innerText);
+	buttons[i].innerText = 'Hello!';
+	alert(buttons[i].innerText);
+	console.log(buttons[i].innerText);
 }
+
